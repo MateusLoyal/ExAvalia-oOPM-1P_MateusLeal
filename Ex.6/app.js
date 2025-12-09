a4b03259529bc37e6120873fd140100b705193e8
Mateus <1258@iai.pt>
2025-12-09 13:12:04 +0000

    Add files via upload

--- Ex.6/app.js
+++ Ex.6/app.js
@@ -2,13 +2,17 @@ alert('Olá, mundo! Vamos fazer contas simples.')
 
 let Pergunta1 = prompt('Que operação é que pretendes realizar?')
 
-if (Pergunta1 = multiplicação ) {
-    alert
+if (Pergunta1 = Multiplicação) {
+    (Resultado = Pergunta2 * Pergunta3) = true
 }
 
 let Pergunta2 = prompt('Insere o primeiro número da operação')
 
-
+if (Pergunta1 = multiplicação) { (Resultado = Pergunta2 * Pergunta3) }
 
 
 let Pergunta3 = prompt('Insere o segundo número da operação')
+
+let Resultado
+
+console.log(`${Pergunta2} * ${Pergunta3} = ${Pergunta2 * Pergunta3}`)
